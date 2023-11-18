6da6e0b66d5853e09ee3fa0cbbfa28da1034c123
OLEKSANDR ISAIEV <nevkluchat@gmail.com>
2023-11-18 10:03:27 +0200

    extend contents

---
+++ SwiftUIAdvancedLearning/SwiftUIAdvancedLearning/SwiftUIAdvancedLearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAdvancedLearningApp.swift
+//  SwiftUIAdvancedLearning
+//
+//  Created by OLEKSANDR ISAIEV on 05.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAdvancedLearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
